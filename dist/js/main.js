3e3a54f0e440ee29722a18811d9d9ca719c2c711
Alexey <SmiteVils@yandex.ru>
2016-07-07 00:04:21 +0300

    fix bugs

--- dist/js/main.js
+++ dist/js/main.js
@@ -7,10 +7,40 @@ $(document).ready(function() {
         }
     });
     //**
+    Selectize.define('hidden_textfield', function(options) {
+            var self = this;
+            this.showInput = function() {
+                 this.$control.css({cursor: 'pointer'});
+                 this.$control_input.css({opacity: 0, position: 'relative', left: self.rtl ? 10000 : -10000 });
+                 this.isInputHidden = false;
+             };
+
+             this.setup_original = this.setup;
+
+             this.setup = function() {
+                  self.setup_original();
+                  this.$control_input.prop("disabled","disabled");
+             }
+    });
+    //**
     $('select').selectize({
         //sortField: 'text',
-        hideSelected: true
+        create: false,
+        hideSelected: true,
+        create: false,
+        scrollDuration: 600,
+        plugins: ['hidden_textfield']
     });
+
+    // $('select').selectize({
+    //     onFocus: function() {
+    //         var input = 'selectize-input input',
+    //               wrapper = 'selectize-input';
+    //         $('.' + input).attr('readonly', true);
+    //         $('.' + input + ', .' + wrapper).css('cursor', 'pointer');
+    //         if((event.keyCode < 13)) alert();
+    //     }
+    // });
     //
     $(document).on('click', '.js-scroll', function(event) {
         event.preventDefault();
