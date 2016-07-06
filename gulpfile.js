e6fa5380c4d052938685ea6887e158a2e6723ade
Alexey <SmiteVils@yandex.ru>
2016-07-07 01:29:07 +0300

    fix gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -7,11 +7,12 @@ var rigger = require('gulp-rigger');
 var autoprefixer = require('gulp-autoprefixer');
 var cssmin = require('gulp-cssmin');
 var rename = require('gulp-rename');
-
 var px2rem = require('gulp-px2rem');
 var px2remOptions = {replace: false};
 var postCssOptions = {map: true};
-
+var del = require('del');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 
@@ -19,17 +20,20 @@ var path = {
   build: {
     html: './dist/',
     js: './dist/js',
-    style: './dist/styles'
+    style: './dist/styles',
+    img: './dist/images'
   },
   src: {
     html: './dev/html/*.html',
     js: './dev/js/*.js',
-    style: './dev/scss/*.scss'
+    style: './dev/scss/*.scss',
+    img: './dev/images/**/*'
   },
   watch: {
     html: './dev/html/**/*.html',
     js: './dev/js/*.js',
-    style: './dev/scss/*.scss'
+    style: './dev/scss/*.scss',
+    img: './dev/images/**/*'
   }
 };
 
@@ -66,6 +70,7 @@ gulp.task('sass', function () {
   gulp.src(path.src.style)
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer())
+    .pipe(gulp.dest(path.build.style))
     .pipe(cssmin())
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest(path.build.style))
@@ -75,6 +80,22 @@ gulp.task('sass', function () {
 gulp.task('notify', function () {
 	gulp.src(path.src.style)
 	.pipe(notify("Откомпилил и собрал!"));
+});
+
+gulp.task('img:build', function() {
+    // delete old files
+    del([path.build.img] + '/**').then(paths => {
+    	//console.log('Deleted files and folders:\n', paths.join('\n'));
+    });
+    //
+	return gulp.src(path.src.img)
+	.pipe(imagemin({
+        optimizationLevel: 3,
+		progressive: true,
+		svgoPlugins: [{removeViewBox: false}],
+		use: [pngquant()]
+	}))
+	.pipe(gulp.dest(path.build.img));
 });
 
 // Watch
